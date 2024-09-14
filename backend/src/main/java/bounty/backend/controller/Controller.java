dfc426ce81c07bd6c3994ae6fdaa3cacaf0a4c6f
William Dale <wd40bug@gmail.com>
2024-09-14 15:48:20 -0500

    dummy actual

--- backend/src/main/java/bounty/backend/controller/Controller.java
+++ backend/src/main/java/bounty/backend/controller/Controller.java
@@ -2,13 +2,17 @@ package bounty.backend.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.util.PropertySource.Comparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import bounty.backend.algorithm.Compatability;
 import bounty.backend.model.Bounty;
 import bounty.backend.model.Hunter;
 import bounty.backend.repository.BountyRepository;
@@ -36,12 +40,22 @@ public class Controller {
   }
 
   @GetMapping("/test")
-  public String test(){
+  public String test() {
     return "Hello Frontend";
   }
 
   @GetMapping("/dummy_data")
-  public List<Bounty> dummy_data(@RequestBody() Hunter hunter){
-    return repository.get_lt_danger(hunter.skill() * 10); 
+  public List<Bounty> dummy_data(@RequestParam(name = "hunter") Hunter hunter) {
+    return repository.get_lt_danger(hunter.skill() * 10);
+  }
+
+  @GetMapping("/feed")
+  public List<Bounty> feed(@RequestParam(name = "hunter") Hunter hunter, @RequestParam(name = "max") int max) {
+    List<Bounty> bounties = repository.get_lt_danger(hunter.skill() * 10);
+
+    var sorted = bounties.stream().sorted(java.util.Comparator.comparing(val -> Compatability.compatability(hunter, val)))
+        .collect(Collectors.toList());
+
+    return sorted;
   }
 }
