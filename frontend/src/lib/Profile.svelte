c5c087fef9da76a93aacae228321c580b99c10a1
ella-n-davis <end41405@gmail.com>
2024-09-15 00:34:49 -0500

    image randomization!

--- frontend/src/lib/Profile.svelte
+++ frontend/src/lib/Profile.svelte
@@ -2,13 +2,35 @@
   import type { Hunter } from "$lib/types";
   export let hunter: Hunter;
   import img from "$lib/assets/Stickman.png";
+  //imgs
+  import person1 from "$lib/assets/result(3).png";
+  import person2 from "$lib/assets/result(2).png";
+  import person3 from "$lib/assets/result(1).png";
+  import hat1 from "$lib/assets/hat3(1).png";
+  import hat2 from "$lib/assets/hat2(1).png";
+  import hat3 from "$lib/assets/hat1(1).png";
+
+  let arr: string[] = [person1, person2, person3];
+  const ind: number = Math.floor(Math.random()* arr.length);
+  const result: string = arr[ind];
+
+  let arr2: string[] = [hat1, hat2, hat3];
+  const ind2: number = Math.floor(Math.random()* arr2.length);
+  const result2: string = arr2[ind2];
 </script>
 
 <div class="profile-card">
   <div class="header">{hunter.name}'s Profile</div>
-  <div class="picture">
-    <img src={img} alt="" />
+  
+  <div class="wrapper">
+    <div class="picture">
+      <img src={result} alt="" />
+    </div>
+    <div class="picture">
+      <img src={result2} alt="" />
+    </div>
   </div>
+
   <div class="info"><strong>Skill:</strong> {hunter.skill}</div>
   <div class="info"><strong>Location:</strong> {hunter.location}</div>
   <div class="info"><strong>Type:</strong> {hunter.type}</div>
@@ -32,11 +54,23 @@
     text-align: center;
   }
 
-  .picture {
+  /* .picture2 {
     width: 100%;
     height: 100%; /* Adjust based on requirement */
-    background-color: #ccc;
+/*     
     margin-bottom: 20px;
+  } */ 
+  .picture {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%; /* Adjust based on requirement */
+    margin-bottom: 20px;
+  }
+
+  .wrapper{
+    position:relative
   }
 
   .info {
