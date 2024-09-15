cb88ffcad27ab86ecf8afa121041addebc9841a9
panconcocacola <41069754+panconcocacola@users.noreply.github.com>
2024-09-14 21:10:29 -0500

    profile.svelte and types.ts updated

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -14,6 +14,14 @@ export type Bounty = {
   styles: string[]
 }
 
+export enum HunterClass {
+  Gunslinger,
+  Cowboy,
+  Sniper,
+  Trickster,
+  Sheriff
+}
+
 export enum Crime {
   Bandit,
   Rustler,
@@ -28,4 +36,28 @@ export enum Condition {
   dead_or_alive
 }
 
-export type Hunter = {} //TODO: Fill this in
+export type Hunter = {
+  id: string,
+  name: string,
+  pic: string,
+  skill: number,
+  location: [number, number],
+  speed: number,
+  draw_speed: number,
+  discernment: number,
+  type: HunterClass
+}
+
+export function getDefaultHunter(): Hunter {
+  return {
+    id: "default-id",
+    name: "John Doe",
+    pic: "https://example.com/default-pic.png",
+    skill: 50,
+    location:[0,0],
+    speed: 5,
+    draw_speed: 3,
+    discernment: 4,
+    type: HunterClass.Cowboy
+  };
+}
