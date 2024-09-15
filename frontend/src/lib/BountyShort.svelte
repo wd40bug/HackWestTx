72d83b63dca70c03fcf2aabbb748b68c23bd1327
ella-n-davis <end41405@gmail.com>
2024-09-15 00:50:59 -0500

    randomized bounty pics

--- frontend/src/lib/BountyShort.svelte
+++ frontend/src/lib/BountyShort.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
-  
+
   import type { Bounty } from "$lib/types";
   export let bounty: Bounty;
+  import bounty1 from "$lib/assets/bounty1.png";
+  import bounty2 from "$lib/assets/bounty2.png";
+  import bounty3 from "$lib/assets/bounty3.png";
+  import bounty4 from "$lib/assets/bounty4.png";
+  import bounty5 from "$lib/assets/bounty5.png";
+  import bounty6 from "$lib/assets/bounty6.png";
 
+  let arr: string[] = [bounty1,bounty2,bounty3,bounty4,bounty5,bounty6];
+  const ind: number = Math.floor(Math.random()* arr.length);
+  const result: string = arr[ind];
 </script>
 
 <article>
-  <img src="Stickman.png" alt="stickman criminal"/>
+  <img class="picture" src={result} alt="criminal"/>
   <h4><strong>$$ {bounty.reward} $$</strong></h4>
   <ul id="identifiers" style="color:rgba(155, 74, 40, 0.877)"> 
       <li><strong>Condition</strong>{bounty.bounty_condition}</li>
@@ -33,6 +42,10 @@
 </article>
 
 <style>
+  .picture{
+    width: 25%;
+    height: 25%;
+  }
 article{
   border: 2px solid black;
   padding: 6px;
